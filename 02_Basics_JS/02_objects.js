de00125f82c40fe6ef8c810bc29eb051dbf8cd71
Abdullah Akasha <121800837+AbdullahAkasha@users.noreply.github.com>
2024-09-12 20:19:23 +0000

    Push

---
+++ 02_Basics_JS/02_objects.js
@@ -0,0 +1,32 @@
+const newuser = {
+    Email: "some@gamil.com",
+    Name: {
+        FullName: {
+            FirstName: "Abdullah",
+            LastName: "Akasha"
+        }
+    }
+}
+
+// console.log(newuser.Name.FullName.LastName)
+
+// const obj1 = {
+//     1: "a",
+//     2: "b"
+// }
+
+// const obj2 = {
+//     3: "c",
+//     4: "d"
+// }
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// console.log(obj3)
+
+// console.log(newuser)
+// console.log(Object.keys(newuser))
+// console.log(Object.values(newuser))
+
+// console.log(newuser.Name.FullName.FirstName)
