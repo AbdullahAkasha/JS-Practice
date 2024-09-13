486a2b587ad5ef0744614527ef8c12978ea9bddc
Abdullah Akasha <121800837+AbdullahAkasha@users.noreply.github.com>
2024-09-13 18:42:26 +0000

    Push

---
+++ 03_Basics_JS/02_Arrow_Functions.js
@@ -0,0 +1,31 @@
+// const user = {
+//     username: "Abdullah",
+//     price: 999,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username} Welcome to website`)
+//     }
+// }
+
+// user.welcomeMessage()
+
+// user.username = "Akasha"
+
+// user.welcomeMessage()
+
+// chai = function(){
+//     console.log(this);
+// }
+
+// chai()
+
+// Arrow Functions
+
+// const chai = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(chai(3 , 5));
+
+const addTow = (num1, num2) => (num1 + num2);
+
+console.log(addTow(5,5));
